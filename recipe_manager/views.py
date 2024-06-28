f7ed408debc831435261965b3de1804a4eed7bf9
Pro Learner <uzumakivaskar123@gmail.com>
2024-06-28 16:36:33 +0545

    Refactor: Use a single template recipe_form.html for create and update recipes

--- recipe_manager/views.py
+++ recipe_manager/views.py
@@ -11,6 +11,8 @@ import sweetify
 
 def upload_recipe(request):
     title = 'LoveRecipes: Add Your Delicious Recipe'
+    subtitle = 'Adding your personal recipes is easy. Add yours to your favorites, share with friends, family, or the LoveRecipes community.'
+    submit_button_text = 'Submit Recipe'
     if request.method == 'POST':
         form = RecipeForm(request.POST, request.FILES)
         if form.is_valid():
@@ -34,11 +36,53 @@ def upload_recipe(request):
             print("Form errors:", form.errors)
     else:
         form = RecipeForm()
-        context = {
-            'form': form,
-            'title': title,
-        }
-    return render(request, 'create_recipes.html', context)
+    context = {
+        'form': form,
+        'title': title,
+        'subtitle': subtitle,
+        'submit_button_text': submit_button_text,
+        'recipe': None,
+    }
+    return render(request, 'recipe_form.html', context)
+
+
+def update_recipe(request, recipe_id):
+    title = 'LoveRecipes: Edit recipe'
+    subtitle = 'Edit details of your recipe below.'
+    submit_button_text = 'Update Recipe'
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            ingredients = form.cleaned_data['ingredients']
+            directions = form.cleaned_data['directions']
+            tags = form.cleaned_data['tags']
+
+            recipe = form.save(commit=False)
+            recipe.set_ingredients(ingredients)
+            recipe.set_directions(directions)
+            recipe.set_tags(tags)
+            recipe.save()
+
+            sweetify.success(
+                request, 'All set! Your recipe has been successfully updated.', timer=3000)
+
+            return redirect('recipe_manager:recipe_details', recipe_id=recipe.id)
+        else:
+            print("Form errors:", form.errors)
+    else:
+        form = RecipeForm(instance=recipe)
+
+    context = {
+        'form': form,
+        'title': title,
+        'subtitle': subtitle,
+        'submit_button_text': submit_button_text,
+        'recipe': recipe,
+    }
+
+    return render(request, 'recipe_form.html', context)
 
 
 def view_recipes(request):
@@ -86,41 +130,6 @@ def recipe_details(request, recipe_id):
     }
 
     return render(request, 'recipe_details.html', context)
-
-
-def update_recipe(request, recipe_id):
-    title = 'LoveRecipes: Edit recipe'
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-
-    if request.method == 'POST':
-        form = RecipeForm(request.POST, request.FILES, instance=recipe)
-        if form.is_valid():
-            ingredients = form.cleaned_data['ingredients']
-            directions = form.cleaned_data['directions']
-            tags = form.cleaned_data['tags']
-
-            recipe = form.save(commit=False)
-            recipe.set_ingredients(ingredients)
-            recipe.set_directions(directions)
-            recipe.set_tags(tags)
-            recipe.save()
-
-            sweetify.success(
-                request, 'All set! Your recipe has been successfully updated.', timer=3000)
-
-            return redirect('recipe_manager:recipe_details', recipe_id=recipe.id)
-        else:
-            print("Form errors:", form.errors)
-    else:
-        form = RecipeForm(instance=recipe)
-
-    context = {
-        'form': form,
-        'recipe': recipe,
-        'title': title
-    }
-
-    return render(request, 'update_recipes.html', context)
 
 
 def delete_recipe(request, recipe_id):
